9e4225f75bedfbfa2efd6c9359bd4841211747d1
melquis1420 <46160910+melquis1420@users.noreply.github.com>
2021-11-26 12:16:13 -0300

    Created the bottom navigation bar and new interfaces.

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/telas/Biblioteca.dart';
+import 'package:youtube/telas/EmAlta.dart';
+import 'package:youtube/telas/Inscricao.dart';
+import 'package:youtube/telas/inicio.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,8 +12,15 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _indiceAtual = 0;
   @override
   Widget build(BuildContext context) {
+    List<Widget> telas = [
+      Inicio(),
+      EmAlta(),
+      Inscricao(),
+      Biblioteca(),
+    ];
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -38,7 +49,35 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      body: Container(),
+      body: telas[_indiceAtual],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _indiceAtual,
+          //indice selecionado
+          onTap: (indice) {
+            setState(() {
+              _indiceAtual = indice;
+            });
+          },
+          type: BottomNavigationBarType.fixed,
+          fixedColor: Colors.red,
+          items: [
+            BottomNavigationBarItem(
+              title: Text("Início"),
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              title: Text("Em alta"),
+              icon: Icon(Icons.whatshot),
+            ),
+            BottomNavigationBarItem(
+              title: Text("Inscrições"),
+              icon: Icon(Icons.subscriptions),
+            ),
+            BottomNavigationBarItem(
+              title: Text("Biblioteca"),
+              icon: Icon(Icons.folder),
+            ),
+          ]),
     );
   }
 }
